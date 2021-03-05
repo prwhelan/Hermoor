99630731f3a575239d657b8455795f0c3b4f23b3
Patty Cakes <pwhelan717@gmail.com>
2021-03-05 15:36:50 -0500

    Keep polling when the server is offline
    When the server goes offline, this crashes (by design), but we actually
    want to keep it going.
    
    Some observations:
    - People usually stop playing for 8+ hours
    - People usually play for longer than 30m
    - When the server goes offline and comes back online, it gets a new IP
    address
    
    Notes:
    - If we fail to call the Valheim server, it's probably just offline.
    Pause for longer (~30m) but then refresh the IP address.

--- src/hermoor.py
+++ src/hermoor.py
@@ -5,13 +5,17 @@ import datetime
 from datetime import timezone
 import time
 
+# When the server is online, run every 5 minutes
 SLEEP_DURATION = 5 * 60
+# When the server is offline, run every 30 minutes
+OFFLINE_SLEEP_DURATION = SLEEP_DURATION * 6
+# If we're failing to call AWS, stop trying and wait for manual intervention.
 awsErrorCount = 0
-valheimErrorCount = 0
 client = boto3.client('cloudwatch')
 
 # Find the valheim server, probably combine this with heimdallr
-serverAddress = boto3.client('ec2').describe_instances(
+def serverAddress():
+    return boto3.client('ec2').describe_instances(
         Filters=[
             {
                 'Name': 'tag:hermoor',
@@ -47,14 +51,14 @@ def publishMetrics(serverName, playerCount):
             e.response['Error']['Message']))
         awsErrorCount+= 1
 
-# Loop indefinitely, unless for some reason we're chaining errors
-while awsErrorCount < 5 and valheimErrorCount < 5:
+# Loop indefinitely, unless for some reason we're chaining errors.
+# Only refresh the server IP Address if we failed to call the server.
+ipAddress = serverAddress()
+while awsErrorCount < 5:
     try:
-        info = a2s.info((serverAddress, 2457))
-        valheimErrorCount = 0
+        info = a2s.info((ipAddress, 2457))
         publishMetrics(info.server_name, info.player_count)
-
-    except Exception as e:
-        print("Error calling the Valheim Server.")
-        valheimErrorCount+= 1
-    time.sleep(SLEEP_DURATION)
+        time.sleep(SLEEP_DURATION)
+    except Exception as valheimServerException:
+        time.sleep(OFFLINE_SLEEP_DURATION)
+        ipAddress = serverAddress()
